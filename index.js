ba24095aff1b5e2f05c6abcef19f9308142b8eeb
bach.dang.hung <bach.dang.hung@sun-asterisk.com>
2023-05-23 21:02:49 +0700

    feat: add env file

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 const { CosmosClient } = require("@azure/cosmos");
+require("dotenv").config();
 
-const endpoint = '';
-const key = '';
-const databaseName = ''
-const containerName = ''
+const endpoint = process.env.END_POINT;
+const key = process.env.KEY;
+const databaseName = process.env.DATABASE_NAME;
+const containerName = process.env.CONTAINER_NAME;
 
 const client = new CosmosClient({ endpoint, key });
 
 async function main() {
-    const { database } = await client.databases.createIfNotExists({ id: databaseName });
-    const { container } = await database.containers.createIfNotExists({ id: containerName });
+  const { database } = await client.databases.createIfNotExists({
+    id: databaseName,
+  });
+  const { container } = await database.containers.createIfNotExists({
+    id: containerName,
+  });
 
-    const { resources } = await container.items
-        .query("SELECT * from c")
-        .fetchAll();
+  const { resources } = await container.items
+    .query("SELECT * from c")
+    .fetchAll();
 
-    for (const resource of resources) {
-        await container.item(resource.id, resource.partitionKey).delete();
-    }
+  for (const resource of resources) {
+    await container.item(resource.id, resource.partitionKey).delete();
+  }
 
-    console.log('done');
+  console.log("done");
 }
 
 main();
